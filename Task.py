b2a638249144210672cbb4e1622f803cc95ec481
DidiPg <122737790+DidiPg@users.noreply.github.com>
2025-05-07 20:29:32 +0200

    Update Task.py

--- Task.py
+++ Task.py
@@ -1,3 +1,4 @@
+"""
 class superhero :
 	def __init__(self, name):
 		self.name = name
@@ -8,3 +9,61 @@ class superhero :
 
 BatMan = superhero("BatMan")
 BatMan.printName()
+"""
+
+"""
+class superhero :
+	def __init__(self, name, power, origin, friends, age):
+		self.name = name
+		self.power = power
+		self.origin = origin
+		self.friends = friends
+	def setName(self, name):
+		self.name = name
+	def printName(self):
+		print(self.name)
+	def printPower(self):
+		print(self.power)
+	def printOrigin(self):
+		print(self.origin)
+	def printFriends(self.friends):
+		print(self.friends)
+	
+
+BatMan = superhero("BatMan", "Strong", "Gotham", "BatGirl")
+BatMan.printName()
+BatMan.printPower()
+
+"""
+"""
+class Superhero:
+    def __init__(self, name, power, origin, friends, age):
+        self.name = name
+        self.power = power
+        self.origin = origin
+        self.friends = friends
+        self.age = age 
+    	
+    def setName(self, name):
+        self.name = name
+    def printName(self):
+        print(self.name)
+    def printPower(self):
+        print(self.power)
+    def printOrigin(self):
+        print(self.origin)
+    def printFriends(self):
+        print(self.friends)
+    def fly(self):
+        print(f"{self.name} Fly in the air")
+    def become_invisible(self):
+        print(f"{self.name} become invisible ")
+    def super_strength(self):
+        print(f"{self.name} can twrow car")
+    def teleport(self):
+        print(f"{self.name} telporte in any place")
+    def shoot_laser_eyes(self):
+        print(f"{self.name} throw laser with eyes")
+
+"""
+
