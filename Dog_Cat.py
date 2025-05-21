0026d057bb892dac25d82177f4705e301eda39fc
DidiPg <122737790+DidiPg@users.noreply.github.com>
2025-05-21 18:01:53 +0200

    Add files via upload

---
+++ Dog_Cat.py
@@ -0,0 +1,28 @@
+class Dog :
+	def __init__(self, color, eye_color, height, length, weight):
+		self.name = name
+		self.color = color
+		self.eye_color = eye_color
+		self.height = height
+		self.length = length
+		self.weight = weight
+	def sit(self):
+		print("The dog sits")
+	def lay_down(self):
+		print("The dog lays downs")
+	def shake(self):
+		print("The dog shakes paw")
+	def come(self):
+		print("The dog comes")
+
+class Cat :
+	def __init__(self, name, color, eye_color, height, length, weight):
+		self.name = name
+		self.color = color
+		self.eye_color = eye_color
+		self.height = height
+		self.length = length
+		self.weight = weight
+
+Aur = Dog("Aurel", "Yellow", "Brown", "17 in", "35 in", "24 pounds")
+Gron = Cat("Goron","Yellow", "Multicolor", "14 in", "25 in", "12 pounds" )
